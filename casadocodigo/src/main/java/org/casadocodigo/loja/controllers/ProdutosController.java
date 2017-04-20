06733d58226589fb98bdd3ce6fa7bde2aa91a9b6
Evandro Carlos Toniati Rivolta <erivolta@uoldiveo.com>
2017-04-20 18:59:15 -0300

    Carregando o projeto

---
+++ casadocodigo/src/main/java/org/casadocodigo/loja/controllers/ProdutosController.java
@@ -0,0 +1,32 @@
+package org.casadocodigo.loja.controllers;
+
+import org.casadocodigo.loja.daos.ProdutoDAO;
+import org.casadocodigo.loja.models.Produto;
+import org.casadocodigo.loja.models.TipoPreco;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ProdutosController {
+
+	@RequestMapping("/produtos/form")
+    public ModelAndView form(){
+
+        ModelAndView modelAndView = new ModelAndView("produtos/form");
+        modelAndView.addObject("tipos", TipoPreco.values());
+
+        return modelAndView;
+    }
+	
+	@Autowired
+    private ProdutoDAO produtoDao;
+	
+	@RequestMapping("/produtos")
+	public String gravar(Produto produto){
+	 System.out.println(produto);
+	 produtoDao.gravar(produto);
+	    return "/produtos/ok";
+	}
+}
