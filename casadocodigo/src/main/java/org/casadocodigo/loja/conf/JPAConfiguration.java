5a359450933e2ea1c7065e8f8bc0293b7cc27714
Evandro Carlos Toniati Rivolta <erivolta@uoldiveo.com>
2017-04-25 11:06:45 -0300

    Corrigindo MySQL

--- casadocodigo/src/main/java/org/casadocodigo/loja/conf/JPAConfiguration.java
+++ casadocodigo/src/main/java/org/casadocodigo/loja/conf/JPAConfiguration.java
@@ -23,8 +23,8 @@ public class JPAConfiguration {
 
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setUsername("root");
-        dataSource.setPassword("");
-        dataSource.setUrl("jdbc:mysql://localhost:3316/casadocodigo");
+        dataSource.setPassword("123456");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/casadocodigo");
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 
         factoryBean.setDataSource(dataSource);
